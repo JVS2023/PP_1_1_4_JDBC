18ff9a8ff386d3a2a6c3d8487ebdc245ffb38e19
Anatoly M <a3588424@gmail.com>
2023-04-19 20:34:46 +0300

    Удалена реализация методов класса UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -13,76 +13,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public static final Connection connection = Util.getConnection();
-
+    
     public UserDaoJDBCImpl() {
-
     }
 
     public void createUsersTable() {
-        String createUsersTable = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL, name VARCHAR(60) NOT NULL, lastname VARCHAR(60) NOT NULL, age TINYINT NOT NULL)";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(createUsersTable);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void dropUsersTable() {
-        String dropUsersTable = "DROP TABLE IF EXISTS users";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(dropUsersTable);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void saveUser(String name, String lastname, byte age) {
-        String saveUser = "INSERT INTO users (name, lastname, age) VALUES (?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(saveUser)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastname);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void removeUserById(long id) {
-        String removeUserById = "DELETE FROM users WHERE id = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(removeUserById)) {
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<User> getAllUsers() {
-        List<User> userList = new ArrayList<>();
-        String getAllUsers = "SELECT * FROM users";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(getAllUsers)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                User user = new User(resultSet.getString("name"),
-                        resultSet.getString("lastname"),
-                        resultSet.getByte("age"));
-                user.setId(resultSet.getLong("id"));
-                userList.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return userList;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String cleanUsersTable = "TRUNCATE TABLE users";
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(cleanUsersTable);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
